de1f94cac5e709f3f5b5d68ad3386df12cb01675
Naf (Nafiyu Murtaza) <144390950+nafna-f@users.noreply.github.com>
2025-01-10 11:32:39 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,12 @@ from flask import Flask, render_template, request, session, redirect, url_for, f
 app = Flask(__name__)    
 app.secret_key = os.urandom(32)
 
+@app.route("/")
+def home():
+    
+
 if __name__ == "__main__": 
     app.debug = True 
     app.run()
+
+
