87e7ea41d50a654a41dda4f9b70d2731b837a55b
Kyle Lee <117378813+kalee88@users.noreply.github.com>
2025-01-10 04:21:51 -0500

    Working App

---
+++ app/__init__.py
@@ -0,0 +1,17 @@
+# Princeden Hom, Kyle Lee, Sascha Gordon-Zolov, Nafiyu Murtaza
+# Team LiveLaughLoveLebron
+# SoftDev
+# p012
+# 2024-1-10
+
+import sqlite3
+import csv
+import os
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+    
+app = Flask(__name__)    
+app.secret_key = os.urandom(32)
+
+if __name__ == "__main__": 
+    app.debug = True 
+    app.run()
