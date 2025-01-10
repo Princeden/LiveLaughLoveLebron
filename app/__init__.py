aedb2c77b4ba4562da2cfcc8690af4ffa0f11b89
Naf (Nafiyu Murtaza) <144390950+nafna-f@users.noreply.github.com>
2025-01-10 11:40:00 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,18 +4,35 @@
 # p012
 # 2024-1-10
 
-import sqlite3
+#import
+import sqlite3 
 import csv
 import os
 from flask import Flask, render_template, request, session, redirect, url_for, flash
-    
+#app
 app = Flask(__name__)    
 app.secret_key = os.urandom(32)
-
+#home
 @app.route("/")
 def home():
-    
-
+    return render_template("home.html") #more vars here soon
+#createGame
+@app.route("/creategame")
+def createGame():
+    return render_template("createGame.html") #more vars soon
+#game
+@app.route("/game")
+def game():
+    return render_template("game.html") #more vars soon
+#settings
+@app.route("/settings")
+def settings():
+    return render_template("settings.html") #more vars soon
+#login and register
+@app.route("/login")
+def login():
+    return render_template("login.html") #more vars soon, setup both login and register here
+#debug
 if __name__ == "__main__": 
     app.debug = True 
     app.run()
