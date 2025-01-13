37952d1fb8f77d3abc15290d7e5bbb4bef2cd0cc
Sascha Gordon-Zolov <agordonzolov50@stuy.edu>
2025-01-13 11:47:07 -0500

    games api

---
+++ app/utils/api.py
@@ -0,0 +1,9 @@
+def getCards(){
+'''
+   Dictionary of the form card = {
+  "value": "number",        Card 1
+  "suit": "SUIT",
+  "value": "number",        Card 2
+  "suit": "SUIT"
+     '''
+}
--- app/utils/game.py
+++ app/utils/game.py
@@ -1,13 +1,12 @@
 class Player:
-    def __init_(self, username, balance):
+    def __init_(self, username, balance, card1, card2):
         self.balance = balance
         self.username = username
-
+        x = getCards()
+        self.card1 =
+        self.card2 = card2
+        Player.getCards()
 class Game:
     def __init__(self, players):
-        
+
 class Table:
-
-
-        
-    
