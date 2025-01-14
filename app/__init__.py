ae4a9a03161846e701d1fec0c38f4b981824f685
Princeden <122395578+Princeden@users.noreply.github.com>
2025-01-14 09:31:12 -0500

    lobby

--- app/__init__.py
+++ app/__init__.py
@@ -38,13 +38,40 @@ def home():
         return render_template('home.html', user=user_data)
     else:
         return render_template('home.html', user=None)
+
+@app.route("/add_to_game", methods=['GET', 'POST'])
+def add_to_game():
+    username = request.form['username']
+    password = request.form['password']
+    conn = get_db_connection()
+    user = conn.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
+    conn.close()
+    if not user:
+        flash('Username does not exist.', 'danger')
+        return render_template("lobby.html", users=users_in_game)
+    elif not check_password_hash(user['password_hash'], password):
+        flash('Incorrect password.', 'danger')
+        return render_template("lobby.html", users=users_in_game)
+    elif username not in users_in_game:
+        users_in_game.append(username)
+        print(users_in_game)
+        return render_template("lobby.html", users=users_in_game)
+    else: 
+        flash('User already in game', 'danger')
+        return render_template("lobby.html", users=users_in_game)
+    return render_template("lobby.html", users=users_in_game)
+
+
 #createGame
 @app.route("/lobby")
 def lobby():
     if 'username' in session:
         if session['username'] not in users_in_game:
             users_in_game.append(session['username'])
-            return render_template("lobby.html", users=users_in_game) #more vars soon
+        print(users_in_game)
+        return render_template("lobby.html", users=users_in_game) #more vars soon
+    else:
+        return "ERROR"
 #game
 @app.route("/game")
 def game():
