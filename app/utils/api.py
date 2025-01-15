ec1f08e72cb17869407587cdc871317e8713bf14
Sascha Gordon-Zolov <agordonzolov50@stuy.edu>
2025-01-15 02:52:01 -0500

    small puish

--- app/utils/api.py
+++ app/utils/api.py
@@ -1,9 +1,8 @@
-def getCards(){
-'''
+def getCards():
+   '''
    Dictionary of the form card = {
   "value1": "number",        Card 1
   "suit1": "SUIT",
   "value2": "number",        Card 2
   "suit2": "SUIT"
      '''
-}
