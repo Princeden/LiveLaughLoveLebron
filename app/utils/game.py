03ede6ae5213088e809b7f5d80a1b6b2ed4719d1
Sascha Gordon-Zolov <agordonzolov50@stuy.edu>
2025-01-15 03:01:16 -0500

    small change

--- app/utils/game.py
+++ app/utils/game.py
@@ -3,10 +3,8 @@ class Player:
         self.balance = balance
         self.username = username
         x = getCards()
-        self.card1 =
+        self.card1 = card1
         self.card2 = card2
         Player.getCards()
-class Game:
-    def __init__(self, players):
-
-class Table:
+#class Table:
+#    def __init(self, players):
